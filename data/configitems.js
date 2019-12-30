ced8e934a34748afa80af0217f8f664f27b6aafe
glengraebner <gdgraebner@gmail.com>
2019-12-29 19:47:06 -0500

    Initial upload

---
+++ data/configitems.js
@@ -0,0 +1,45 @@
+var configitems=
+[
+   {
+      'HSC' : '1112134',
+      'Title' : 'Navigation System',
+      'TR System' : 'X',
+      'Config Status' : 'Active',
+   },
+   {
+      'HSC' : '11121341',
+      'Title' : 'Navigation Computers',
+      'TR System' : '',
+      'Config Status' : 'Active',
+   },
+   {
+      'HSC' : '11121342',
+      'Title' : 'Navigation Harddrives',
+      'TR System' : '',
+      'Config Status' : 'Active',
+   },
+   {
+      'HSC' : '11121343',
+      'Title' : 'Navigation Software',
+      'TR System' : '',
+      'Config Status' : 'Archived',
+   },
+   {
+      'HSC' : '11121343',
+      'Title' : 'Navigation Software',
+      'TR System' : '',
+      'Config Status' : 'Active',
+   },
+   {
+      'HSC' : '1134221',
+      'Title' : 'External Handling System',
+      'TR System' : 'X',
+      'Config Status' : 'Active',
+   },
+   {
+      'HSC' : '1225774',
+      'Title' : 'Launch System',
+      'TR System' : 'X',
+      'Config Status' : 'Active',
+   },
+]
