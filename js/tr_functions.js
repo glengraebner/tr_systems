a08be8f15c1dfd2b6ecf7d9df21a475fc99c126a
glengraebner <gdgraebner@gmail.com>
2019-12-30 00:53:59 -0500

    Add files via upload

--- js/tr_functions.js
+++ js/tr_functions.js
@@ -2,7 +2,7 @@ function loadTR()
 {   
     try
     {      
-        redisplayTMOF();
+        redisplayTR();
         loadConfigItems(configitems);
     }
     catch(ex)
@@ -20,15 +20,20 @@ function loadConfigItems(data) {
     }
 }
 
-function redisplayTMOF()
+function redisplayTR()
 {
     try
     {
-        document.getElementById('div_main').style.width = '700px';
-        document.getElementById('div_top').style.width = '650px';
+        var winwidth = window.innerWidth;
+        var winheight = window.innerHeight;
+        var divtopwidth = document.getElementById('div_top').offsetWidth;
+        var divtopwidth = document.getElementById('div_top').offsetHeight;
+        
+        document.getElementById('div_main').style.width = winwidth.toString() + 'px';
+        document.getElementById('div_top').style.width = (winwidth-50).toString() + 'px';
         document.getElementById('div_top_left').style.width = '300px';
         document.getElementById('div_top_right').style.width = '300px';
-        document.getElementById('div_bottom').style.width = '650px';
+        document.getElementById('div_bottom').style.width = (winwidth-50).toString() + 'px';
         document.getElementById('div_bottom_left').style.width = '300px';
         document.getElementById('div_bottom_right').style.width = '300px';        
     }
